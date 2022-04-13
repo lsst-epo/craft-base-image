203227980bd6966acf16bf7ac97033869cd8b3a0
Blake Mason <bmason@lsst.org>
2022-04-13 12:24:45 -0700

    [C] Streamline craft installs, updates, and migrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
 set -e
 
-# Install Craft plugins
+# Install composer packages
+./craft update/composer-install
 
-if [ -f composer.json ]; then
-
-    dependencies=$(cat composer.json |
-        jq '.require' |
-        jq --compact-output 'keys' |
-        tr -d '[]"' | tr ',' '\n')
-
-    for package in ${dependencies}; do
-
-        echo $package
-
-        vendor=$(awk -F '[\/:]+' '{print $1}' <<<$package)
-        packageName=$(awk -F '[\/:]+' '{print $2}' <<<$package)
-        isCraftPlugin=$(cat vendor/$vendor/$packageName/composer.json | jq '.type == "craft-plugin"')
-
-        if [ "$isCraftPlugin" = true ]; then
-            handle=$(cat vendor/$vendor/$packageName/composer.json | jq -r '.extra.handle')
-            ./craft plugin/install $handle
-        fi
-    done
-fi
-
-# Ensure any pending migrations are run
+# Run pending migrations
 ./craft migrate/all --no-backup
 
-# Ensure the project config is set correctly
+# Apply changes from project config
 ./craft project-config/apply
 
 # https://docs.docker.com/engine/reference/builder/#exec-form-entrypoint-example
