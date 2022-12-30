1f8f96f076a9c1610862077b6dc41dd7c801f6aa
Blake Mason <bmason@lsst.org>
2022-12-30 14:00:58 -0800

    [C] Structural and content changes seperated into their own steps

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,19 @@
 set -e
 
 # Install composer packages
-./craft update/composer-install
+./craft update/composer-install --interactive=0
 
-# Run pending migrations
-./craft migrate/all --no-backup
+# Run pending structural migrations
+./craft migrate/all --no-backup --no-content --interactive=0
 
-# Apply changes from project config (unless ENVIRONMENT=dev)
-# --force will apply everything from the project config files
-[ "$ENVIRONMENT" != "dev" ] && ./craft project-config/apply --force 
+# Apply changes from project config yaml files
+./craft project-config/apply --force
+
+# Run pending content migrations
+./craft migrate --track=content --interactive=0
+
+# Clear caches
+./craft clear-caches/all
 
 # https://docs.docker.com/engine/reference/builder/#exec-form-entrypoint-example
 exec "$@"
